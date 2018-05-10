5f73e594a5793d99c9c629240ec84f65af2ade82
Andrej Badin <contact@andrejbadin.com>
2018-05-10 15:46:01 +0200

    Fix exports

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-module.exports.rewireLingui = function rewireLingui(config, env, options = {}) {
-    config = injectBabelPlugin('@lingui/babel-plugin-transform-js', config);
-    config = injectBabelPlugin('@lingui/babel-plugin-transform-react', config);
-    return config;
+function createLinguiRewire(options = {}) {
+  return function(config, env) {
+    return rewireLingui(config, env, options);
+  };
 }
 
-module.exports.createLinguiRewire = function createLinguiRewire(options = {}) {
-    return function(config, env) {
-        return rewireLingui(config, env, options);
-    }
+function rewireLingui(config, env, options = {}) {
+  config = injectBabelPlugin('@lingui/babel-plugin-transform-js', config);
+  config = injectBabelPlugin('@lingui/babel-plugin-transform-react', config);
+  return config;
 }
+
+module.exports = {
+  rewireLingui,
+  createLinguiRewire,
+};
